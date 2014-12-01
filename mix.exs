5e928aa31b73a712b0249d97cd3e0d7242a5fa42
Chris Ertel <chris@kedagital.com>
2014-12-01 02:05:22 -0600

    Add package information for first release on hex.

--- mix.exs
+++ mix.exs
@@ -5,12 +5,29 @@ defmodule Graphmath.Mixfile do
     [app: :graphmath,
      version: "0.1.0",
      elixir: "~> 1.0",
+     description: description,
+     package: package,
      deps: deps]
   end
 
   def application, do: []
 
+  defp description do
+    """
+    Graphmath is a library for doing 2D and 3D mathemtical operations.
+    """
+  end
+
   defp deps do
     []
   end
+
+  defp package do
+    [
+      contributors: ["Chris Ertel"],
+      licenses: ["Public Domain (unlicense)", "WTFPL", "New BSD"],
+      links: %{"GitHub" => "https://github.com/crertel/graphmath"}
+    ]
+  end
+
 end
