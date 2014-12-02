5553f5091bc093f0e165b8f729598bcf34c3fa49
Chris Ertel <chris@kedagital.com>
2014-12-02 00:20:55 -0600

    Bump Hex number.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Graphmath.Mixfile do
 
   def project do
     [app: :graphmath,
-     version: "0.2.2",
+     version: "0.2.4",
      elixir: "~> 1.0",
      description: description,
      package: package,
