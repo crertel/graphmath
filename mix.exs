9a8cdb9df4d8ab204a0d5a64909e01e1d0a75617
Chris Ertel <chris@kedagital.com>
2014-12-02 23:55:26 -0600

    Bump hex version number.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Graphmath.Mixfile do
 
   def project do
     [app: :graphmath,
-     version: "0.2.6",
+     version: "0.2.8",
      elixir: "~> 1.0",
      description: description,
      package: package,
