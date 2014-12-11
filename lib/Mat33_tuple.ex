41e1fb6a71bc5c19aad099f4d7a63ad15e4e1250
Chris Ertel <chris@kedagital.com>
2014-12-11 01:26:22 -0600

    Add 3x3 matrix addition and test functions.

--- lib/Mat33_tuple.ex
+++ lib/Mat33_tuple.ex
@@ -6,10 +6,14 @@ defmodule Graphmath.Mat33.Tuple do
     This submodule handles 3x3 matrices using tuples of floats.
     """
 
+    @type mat33 :: { float, float, float,
+                     float, float, float,
+                     float, float, float }
+
     @doc"""
     `identity()` is used to create a 3x3 identity matrix.
     """
-    @spec identity() :: {float, float, float, float, float, float, float, float, float }
+    @spec identity() :: mat33
     def identity() do
         { 1, 0, 0,
           0, 1, 0,
@@ -19,11 +23,29 @@ defmodule Graphmath.Mat33.Tuple do
     @doc"""
     `zero()` is used to create a 3x3 zero matrix.
     """
-    @spec zero() :: {float, float, float, float, float, float, float, float, float }
+    @spec zero() :: mat33
     def zero() do
         { 0, 0, 0,
           0, 0, 0,
           0, 0, 0 }
+    end
+
+    @doc"""
+    `add(A,B)` is used to add one 3x3 matrix to another.
+    """
+    @spec add( mat33, mat33) :: mat33
+    def add( a, b ) do
+        { a11, a12, a13,
+          a21, a22, a23,
+          a31, a32, a33 } = a
+
+        { b11, b12, b13,
+          b21, b22, b23,
+          b31, b32, b33 } = b
+
+        { a11 + b11, a12 + b12, a13 + b13,
+          a21 + b21, a22 + b22, a23 + b23,
+          a31 + b31, a32 + b32, a33 + b33 }
     end
     
 end
--- test/tuple/mat33/mat33_tuple_creation_test.exs
+++ test/tuple/mat33/mat33_tuple_creation_test.exs
@@ -12,4 +12,23 @@ defmodule Graphmath.Mat33.Tuple.Create_Mat33 do
   test "zero returns {0,0,0,0,0,0,0,0,0}" do
     assert {0,0,0,0,0,0,0,0,0} == Graphmath.Mat33.Tuple.zero()
   end
+  
+  @tag :mat33
+  @tag :add
+  test "add( I, 0 ) returns I" do
+    assert { 1, 0, 0,
+             0, 1, 0,
+             0, 0, 1 } == Graphmath.Mat33.Tuple.add( {1,0,0,0,1,0,0,0,1},
+                                                     {0,0,0,0,0,0,0,0,0} )
+  end
+  
+  @tag :mat33
+  @tag :add
+  test "add( [1:1:9], [10:10:90] ) returns I" do
+    assert { 11, 22, 33,
+             44, 55, 66,
+             77, 88, 99 } == Graphmath.Mat33.Tuple.add( {1,2,3,4,5,6,7,8,9},
+                                                        {10,20,30,40,50,60,70,80,90} )
+  end
+
 end
