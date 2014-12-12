60794a27dc4fd0d0fe03fdf62c75689c2c4ab00e
Chris Ertel <chris@kedagital.com>
2014-12-12 00:46:40 -0600

    Add mat33 multiplication and test functions.

--- lib/Mat33_tuple.ex
+++ lib/Mat33_tuple.ex
@@ -79,4 +79,33 @@ defmodule Graphmath.Mat33.Tuple do
           a21 * k, a22 * k, a23 * k,
           a31 * k, a32 * k, a33 * k }
     end
+
+    @doc"""
+    `multiply( a, b )` multiply two matrices a and b together.
+    """
+    @spec multiply( mat33, mat33 ) :: mat33
+    def multiply( a, b ) do
+        { a11, a12, a13,
+          a21, a22, a23,
+          a31, a32, a33 } = a
+
+        { b11, b12, b13,
+          b21, b22, b23,
+          b31, b32, b33 } = b
+
+        {
+            (a11*b11) + (a12*b21) + (a13*b31),
+            (a11*b12) + (a12*b22) + (a13*b32),
+            (a11*b13) + (a12*b23) + (a13*b33),
+
+            (a21*b11) + (a22*b21) + (a23*b31),
+            (a21*b12) + (a22*b22) + (a23*b32),
+            (a21*b13) + (a22*b23) + (a23*b33),
+
+            (a31*b11) + (a32*b21) + (a33*b31),
+            (a31*b12) + (a32*b22) + (a33*b32),
+            (a31*b13) + (a32*b23) + (a33*b33)
+        }
+    end
+
 end
---
+++ test/tuple/mat33/mat33_tuple_multiplication_test.exs
@@ -0,0 +1,48 @@
+defmodule Graphmath.Mat33.Tuple.Multiply_Mat33 do
+  use ExUnit.Case
+  
+  @tag :mat33
+  @tag :multiply
+  test "multiply( I, 0 ) returns 0" do
+    assert { 0, 0, 0,
+             0, 0, 0,
+             0, 0, 0 } == Graphmath.Mat33.Tuple.multiply( {1,0,0,0,1,0,0,0,1},
+                                                     {0,0,0,0,0,0,0,0,0} )
+  end
+  
+  @tag :mat33
+  @tag :multiply
+  test "multiply( I, I ) returns I" do
+    assert { 1, 0, 0,
+             0, 1, 0,
+             0, 0, 1 } == Graphmath.Mat33.Tuple.multiply( {1,0,0,0,1,0,0,0,1},
+                                                     {1,0,0,0,1,0,0,0,1} )
+  end
+
+  @tag :mat33
+  @tag :multiply
+  test "multiply( 1:1:9, I ) returns 1:1:9" do
+    assert { 1, 2, 3,
+             4, 5, 6,
+             7, 8, 9 } == Graphmath.Mat33.Tuple.multiply( {1,2,3,4,5,6,7,8,9},
+                                                     {1,0,0,0,1,0,0,0,1} )
+  end
+
+  @tag :mat33
+  @tag :multiply
+  test "multiply( 1:1:9, 10:1:18 )" do
+    assert { 84, 90, 96,
+             201, 216, 231,
+             318, 342, 366 } == Graphmath.Mat33.Tuple.multiply( {1,2,3,4,5,6,7,8,9},
+                                                          {10,11,12,13,14,15,16,17,18} )
+  end
+
+  @tag :mat33
+  @tag :multiply
+  test "multiply( 10:1:18, 1:1:9 )" do
+    assert { 138, 171, 204,
+             174, 216, 258,
+             210, 261, 312 } == Graphmath.Mat33.Tuple.multiply( {10,11,12,13,14,15,16,17,18},
+                                                                {1,2,3,4,5,6,7,8,9} )
+  end
+end
