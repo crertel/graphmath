c34187c0ab5a0b71a17c3e01c87cce21f735885c
Chris Ertel <chris@kedagital.com>
2015-01-14 21:49:52 -0600

    Bumping hex version.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Graphmath.Mixfile do
 
   def project do
     [app: :graphmath,
-     version: "0.4.1",
+     version: "0.4.2",
      elixir: "~> 1.0",
      description: description,
      package: package,
