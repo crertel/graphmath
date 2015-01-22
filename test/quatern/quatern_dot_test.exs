ddd85299d369bedf70a851177465cf2358f38927
Ivan Miranda <ivan.i.n@hotmail.com>
2015-01-22 21:53:44 -0200

    Tests on quaternions

---
+++ test/quatern/quatern_dot_test.exs
@@ -0,0 +1,9 @@
+defmodule GraphmathTest.Quatern.Dot_Quatern do
+  use ExUnit.Case
+
+  @tag :quatern
+  @tag :dot
+  test "dot({1,2,3,4},{5,6,7,8}) returns 70" do
+    assert 70 == Graphmath.Quatern.dot({1,2,3,4},{5,6,7,8})
+  end
+end
