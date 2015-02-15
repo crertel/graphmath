504fb836a02a676837d5015cdd83c9f2dd0e899e
Chris Ertel <chris@kedagital.com>
2015-02-14 18:02:40 -0600

    Bump version number due to bugfix.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Graphmath.Mixfile do
 
   def project do
     [app: :graphmath,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.0",
      description: description,
      package: package,
