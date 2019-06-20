09a3a11290693af3928f4272af5af05c55c46823
Chris Ertel <chris@kedagital.com>
2019-06-19 23:33:22 -0400

    Split out Quatern.to_rotation_matrix/1 into 3x3 and 4x4 forms.

--- test/quatern/quatern_to_matrix_test.exs
+++ test/quatern/quatern_to_matrix_test.exs
@@ -4,16 +4,32 @@ defmodule GraphmathTest.Quatern.ToMatrixQuatern do
   @tag :quatern
   @tag :to_matrix
   # values obtained through another calculator
-  test "to_rotation_matrix({2, 0.25, -0.5, 0.25}) returns {0.375,-1.25,-1.875,0.75,0.75,-1.25,2.125,0.75,0.375}" do
+  test "to_rotation_matrix_33({2, 0.25, -0.5, 0.25}) returns {0.375,-1.25,-1.875,0.75,0.75,-1.25,2.125,0.75,0.375}" do
     assert {0.375, -1.25, -1.875, 0.75, 0.75, -1.25, 2.125, 0.75, 0.375} ==
-             Graphmath.Quatern.to_rotation_matrix({2, 0.25, -0.5, 0.25})
+             Graphmath.Quatern.to_rotation_matrix_33({2, 0.25, -0.5, 0.25})
   end
 
   @tag :quatern
   @tag :to_matrix
   # values obtained through another calculator
-  test "to_rotation_matrix({2, -0.25, 0.5, -0.25}) returns {0.375,0.75,2.125,-1.25,0.75,0.75,-1.875,-1.25,0.375}" do
+  test "to_rotation_matrix_33({2, -0.25, 0.5, -0.25}) returns {0.375,0.75,2.125,-1.25,0.75,0.75,-1.875,-1.25,0.375}" do
     assert {0.375, 0.75, 2.125, -1.25, 0.75, 0.75, -1.875, -1.25, 0.375} ==
-             Graphmath.Quatern.to_rotation_matrix({2, -0.25, 0.5, -0.25})
+             Graphmath.Quatern.to_rotation_matrix_33({2, -0.25, 0.5, -0.25})
+  end
+
+  @tag :quatern
+  @tag :to_matrix
+  # values obtained through another calculator
+  test "to_rotation_matrix_44({2, 0.25, -0.5, 0.25}) returns {0.375,-1.25,-1.875,0.75,0.75,-1.25,2.125,0.75,0.375}" do
+    assert {0.375, -1.25, -1.875, 0.0, 0.75, 0.75, -1.25, 0.0, 2.125, 0.75, 0.375, 0.0, 0.0, 0.0, 0.0, 1.0} ==
+             Graphmath.Quatern.to_rotation_matrix_44({2, 0.25, -0.5, 0.25})
+  end
+
+  @tag :quatern
+  @tag :to_matrix
+  # values obtained through another calculator
+  test "to_rotation_matrix_44({2, -0.25, 0.5, -0.25}) returns {0.375,0.75,2.125,-1.25,0.75,0.75,-1.875,-1.25,0.375}" do
+    assert {0.375, 0.75, 2.125, 0.0, -1.25, 0.75, 0.75, 0.0, -1.875, -1.25, 0.375, 0.0, 0.0, 0.0, 0.0, 1.0} ==
+             Graphmath.Quatern.to_rotation_matrix_44({2, -0.25, 0.5, -0.25})
   end
 end
