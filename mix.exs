faeba41a8e12a7cb246c9f4b959e0bc2718e06ea
crertel <chris@kedagital.com>
2021-06-15 00:33:03 -0500

    Ensure inclusion of test helpers for doc coverage in CI.

--- mix.exs
+++ mix.exs
@@ -24,6 +24,7 @@ defmodule Graphmath.Mixfile do
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:docs), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
